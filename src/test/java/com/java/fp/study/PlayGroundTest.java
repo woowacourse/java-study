43af98732e717989db2033037152bdefa3a8d7be
woowahanCU <brainbackdoor@woowahan.com>
2019-06-12 12:30:06 +0900

    feat(playground): Add an example to learn Lazy Evaluation

--- src/test/java/com/java/fp/study/PlayGroundTest.java
+++ src/test/java/com/java/fp/study/PlayGroundTest.java
@@ -2,9 +2,12 @@ package com.java.fp.study;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -53,5 +56,63 @@ class PlayGroundTest {
         return list.stream()
                 .filter(condition)
                 .collect(toList());
+    }
+
+    @Test
+    public void Eager_Evaluation_테스트() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<String> result = new ArrayList<>();
+
+        for (Integer number : numbers) {
+            log("starting", number);
+            log("filtering", number);
+
+            if (number > 3) {
+                log("post filtering", number);
+                number = number * 2;
+
+                String convertValue = "#" + number;
+                log("post converting", number);
+
+                result.add(convertValue);
+                continue;
+            }
+        }
+
+        log("Invoking terminal method count.\n");
+        log("The count is", result.size());
+    }
+
+    @Test
+    public void Lazy_Evaluation_테스트() {
+        Stream stream = Stream.of(1, 2, 3, 4, 5)
+                .peek(i -> log("starting", i))
+                .filter(i -> {
+                    log("filtering", i);
+                    return i > 3;
+                })
+                .peek(i -> log("post filtering", i))
+                .map(v -> v * 2)
+                .map(i -> "#" + i)
+                .peek(i -> log("post converting", i));
+
+        log("Invoking terminal method count.\n");
+        log("The count is", stream.count());
+    }
+
+    public static void log(Object... objects) {
+        String now = LocalDateTime.now().toString();
+
+        for (Object object : objects) {
+            now += " - " + object.toString();
+        }
+
+        System.out.println(now);
+
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
